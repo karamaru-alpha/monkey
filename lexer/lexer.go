5a41314602e52dcb50ae9c5d4068a3cf9cf2c12f
karamaru-alpha <mrnk3078@gmail.com>
2022-09-03 21:16:47 +0900

    tweak

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,8 +7,8 @@ import (
 type Lexer struct {
 	input        string
 	position     int  // 入力における現在の位置
-	readPosition int  // これから読み込む位置
 	ch           byte // 現在検査中の文字
+	readPosition int  // 次読み込む位置(position+1)
 }
 
 func New(input string) *Lexer {
