ff704706fe4b9b5e2ec7f92da8151471f88c714b
karamaru-alpha <mrnk3078@gmail.com>
2022-09-04 14:48:23 +0900

    tweak: early return

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -28,60 +28,61 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	l.readChar()
 	l.skipWhiteSpace()
-	var tok token.Token
 	switch l.ch {
 	case '=':
 		if l.peekChar() == '=' {
 			l.readChar()
-			tok = token.Token{Type: token.EQ, Literal: "=="}
+			return token.Token{Type: token.EQ, Literal: "=="}
 		} else {
-			tok = token.New(token.ASSIGN, l.ch)
+			return token.New(token.ASSIGN, l.ch)
 		}
 	case '!':
 		if l.peekChar() == '=' {
 			l.readChar()
-			tok = token.Token{Type: token.NOT_EQ, Literal: "!="}
+			return token.Token{Type: token.NOT_EQ, Literal: "!="}
 		} else {
-			tok = token.New(token.BANG, l.ch)
+			return token.New(token.BANG, l.ch)
 		}
 	case ';':
-		tok = token.New(token.SEMICOLUN, l.ch)
+		return token.New(token.SEMICOLUN, l.ch)
 	case '(':
-		tok = token.New(token.LPAREN, l.ch)
+		return token.New(token.LPAREN, l.ch)
 	case ')':
-		tok = token.New(token.RPAREN, l.ch)
+		return token.New(token.RPAREN, l.ch)
 	case ',':
-		tok = token.New(token.COMMA, l.ch)
+		return token.New(token.COMMA, l.ch)
 	case '+':
-		tok = token.New(token.PLUS, l.ch)
+		return token.New(token.PLUS, l.ch)
 	case '-':
-		tok = token.New(token.MINUS, l.ch)
+		return token.New(token.MINUS, l.ch)
 	case '*':
-		tok = token.New(token.ASTERISK, l.ch)
+		return token.New(token.ASTERISK, l.ch)
 	case '/':
-		tok = token.New(token.SLASH, l.ch)
+		return token.New(token.SLASH, l.ch)
 	case '<':
-		tok = token.New(token.LT, l.ch)
+		return token.New(token.LT, l.ch)
 	case '>':
-		tok = token.New(token.GT, l.ch)
+		return token.New(token.GT, l.ch)
 	case '{':
-		tok = token.New(token.LBRACE, l.ch)
+		return token.New(token.LBRACE, l.ch)
 	case '}':
-		tok = token.New(token.RBRACE, l.ch)
+		return token.New(token.RBRACE, l.ch)
 	case 0:
-		tok = token.Token{Type: token.EOF, Literal: ""}
+		return token.Token{Type: token.EOF, Literal: ""}
 	default:
+		var tok token.Token
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
-		} else if isDigit(l.ch) {
+			return tok
+		}
+		if isDigit(l.ch) {
 			tok.Type = token.INT
 			tok.Literal = l.readNumber()
-		} else {
-			tok = token.New(token.INLLEGAL, l.ch)
+			return tok
 		}
+		return token.New(token.INLLEGAL, l.ch)
 	}
-	return tok
 }
 
 func (l *Lexer) skipWhiteSpace() {
