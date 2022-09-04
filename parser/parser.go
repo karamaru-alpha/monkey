26e980de369a4e4c870dc591255706644d9cb769
karamaru-alpha <mrnk3078@gmail.com>
2022-09-04 15:56:31 +0900

    add: add return parser

--- parser/parser.go
+++ parser/parser.go
@@ -47,6 +47,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.currentToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -72,6 +74,16 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		p.nextToken()
 	}
 
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	var stmt = &ast.ReturnStatement{Token: p.currentToken}
+
+	p.nextToken()
+	for p.currentToken.Type != token.SEMICOLUN {
+		p.nextToken()
+	}
 	return stmt
 }
 
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,25 +1,71 @@
 package parser
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/karamaru-alpha/monkey/ast"
 	"github.com/karamaru-alpha/monkey/lexer"
+	"github.com/karamaru-alpha/monkey/token"
 )
 
 func TestParser_LetStatement(t *testing.T) {
-	input := `
-let x = 5;
+	input := `let x = 5;
 let y = 10;
-let foobar = 838383;
 `
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
 	checkParseError(t, p)
 
-	fmt.Println(program.Statements[0].(*ast.LetStatement).Name.TokenLiteral())
+	var tests = []*ast.LetStatement{
+		{
+			Token: token.Token{Type: token.LET, Literal: "let"},
+			Name: &ast.Identifier{
+				Token: token.Token{Type: token.IDENT, Literal: "x"},
+				Value: "x",
+			},
+			Value: nil,
+		},
+		{
+			Token: token.Token{Type: token.LET, Literal: "let"},
+			Name: &ast.Identifier{
+				Token: token.Token{Type: token.IDENT, Literal: "y"},
+				Value: "y",
+			},
+			Value: nil,
+		},
+	}
+
+	for i, tt := range tests {
+		assert.Equal(t, tt, program.Statements[i].(*ast.LetStatement))
+	}
+}
+
+func TestParser_ReturnStatement(t *testing.T) {
+	input := `return 5;
+return add(1, 2);
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParseError(t, p)
+
+	var tests = []*ast.ReturnStatement{
+		{
+			Token: token.Token{Type: token.RETURN, Literal: "return"},
+			Value: nil,
+		},
+		{
+			Token: token.Token{Type: token.RETURN, Literal: "return"},
+			Value: nil,
+		},
+	}
+
+	for i, tt := range tests {
+		assert.Equal(t, tt, program.Statements[i].(*ast.ReturnStatement))
+	}
 }
 
 func checkParseError(t *testing.T, p *Parser) {
