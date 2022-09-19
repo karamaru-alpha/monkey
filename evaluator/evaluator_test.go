7306f9c5a554715e90236017b0ab18745eac51d0
karamaru-alpha <mrnk3078@gmail.com>
2022-09-19 13:21:36 +0900

    add: infix operator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -35,6 +35,10 @@ func TestEval_BooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
 	}
 
 	for _, tt := range tests {
